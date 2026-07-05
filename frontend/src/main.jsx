928f1d91eb4dffabd11b58993c9cd1e68a7468cc
Bhaveshbharti8986 <bhaveshbharti8986@gmail.com>
2026-07-05 12:17:37 +0530

    Add ToastListener component to main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import ToastListener from "./components/layout/ToastListener.jsx";
 import "./index.css";
 import { registerSW } from "virtual:pwa-register";
 
@@ -8,6 +9,7 @@ registerSW({ immediate: true });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <ToastListener />
     <App />
   </React.StrictMode>
 );
